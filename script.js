e13c1722c840a39e16897101a8bbe725df9c5a80
Hemstreet <jonhemstreet@gmail.com>
2017-04-24 01:06:16 -0600

    Cron job with text notifications

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let investorsHub = require('./lib/investorsHub');
+let investorsHub = require('./lib/investorsHub'),
+    notification = require('./lib/notification'),
+    CronJob = require('cron').CronJob;
 
-// Promise interface
-investorsHub.getBreakoutBoard().then(data => {
-    console.log(data);
-});
+new CronJob('0 16 * * *', () => {
+    investorsHub.getBreakoutBoard().then(data => {
+        console.log(data);
+        notification.sendMessage('Stock market script finished scraping');
+    });
+}, null, true, 'America/New_York');

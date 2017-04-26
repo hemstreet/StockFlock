1221d8476770e24588977b7a0eaa51d7cdcbc16c
Hemstreet <jonhemstreet@gmail.com>
2017-04-25 23:24:49 -0600

    db setup

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let investorsHub = require('./lib/investorsHub'),
     notification = require('./lib/notification'),
     CronJob = require('cron').CronJob;
 
-new CronJob('0 16 * * *', () => {
-    investorsHub.getBreakoutBoard().then(data => {
-        console.log(data);
-        notification.sendMessage('Stock market script finished scraping');
-    });
-}, null, true, 'America/New_York');
+investorsHub.getBreakoutBoard().then(data => {
+    console.log(data);
+    notification.sendMessage('Stock market script finished scraping');
+});
+// new CronJob('0 16 * * *', () => {
+//     investorsHub.getBreakoutBoard().then(data => {
+//         console.log(data);
+//         notification.sendMessage('Stock market script finished scraping');
+//     });
+// }, null, true, 'America/New_York');

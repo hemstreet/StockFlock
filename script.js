ffa33f5b269460d714dfd0832f6eaefadaae32b9
Hemstreet <jonhemstreet@gmail.com>
2017-04-26 19:29:18 -0600

    Stock market postsToday over 50 and send message

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 let investorsHub = require('./lib/investorsHub'),
     notification = require('./lib/notification'),
+    _ = require('underscore'),
     CronJob = require('cron').CronJob;
 
-investorsHub.getBreakoutBoard().then(data => {
-    console.log(data);
-    notification.sendMessage('Stock market script finished scraping');
-});
-// new CronJob('0 16 * * *', () => {
-//     investorsHub.getBreakoutBoard().then(data => {
-//         console.log(data);
-//         notification.sendMessage('Stock market script finished scraping');
-//     });
-// }, null, true, 'America/New_York');
+new CronJob('0 16 * * *', () => {
+    investorsHub.getBreakoutBoard().then((data) => {
+        let flaggedTickers = [];
+        _.each(data, tickerObj => {
+            try {
+                let length = tickerObj.scrapes.length;
+
+                if (length > 1) {
+                    let last = tickerObj.scrapes[length - 1],
+                        penultimate = tickerObj.scrapes[length - 2],
+                        postsToday = penultimate.postsToday - last.postsToday,
+                        ticker = last.ticker;
+
+                    if (postsToday > 50) {
+                        flaggedTickers.push({
+                            ticker,
+                            postsToday
+                        })
+                    }
+                }
+            } catch(err) {
+                console.log(err);
+            }
+        });
+
+        let message = "";
+
+        if (flaggedTickers > 0) {
+            flaggedTickers.each(tickerObj => {
+                message += `${tickerObj.ticker} had ${tickerObj.postsToday}\n`;
+            });
+
+            notification.sendMessage(`
+            Market Scrape Finished\n${message}
+        `);
+        }
+        else
+        {
+            notification.sendMessage(`
+            Market Scrape Finished\nNo tickers to watch :(
+        `);
+        }
+    });
+}, null, true, 'America/New_York');
